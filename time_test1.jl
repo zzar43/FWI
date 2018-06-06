8fd74926c429d0d691a4ea0c42fb6de6f1821a6d
zzar43 <lida7980@gmail.com>
2018-06-06 12:34:25 -0600

    parallel1

---
+++ time_test1.jl
@@ -0,0 +1,35 @@
+
+# This file is for check the wave solver and sensitivity in time domain.
+# Before run this file, model_parameter.jl should be checked.
+# There should only be one source. Receiver can be multiple, but suggest test start from 1.
+# The vel_true and vel_init may be different.
+# For the source function, both ricker wavelet and sine wave can be used.
+
+# include("model_parameter.jl");
+# include("2d_wave_solver.jl");
+
+# Make data
+ind_source = 1;
+@time true_wavefield, received_data = wave_solver_2d_pml(vel_true,Nx,Ny,h,Nt,dt,pml_len,pml_alpha,source_coor[ind_source,:]',source_vec[ind_source,:]',receiver_coor);
+# draw_real(true_wavefield[:,:,300])
+
+# Forward and backward
+@time forward_wavefield, received_data_forward = wave_solver_2d_pml(vel_init,Nx,Ny,h,Nt,dt,pml_len,pml_alpha,source_coor[ind_source,:]',source_vec[ind_source,:]',receiver_coor);
+# draw_real(forward_wavefield[:,:,300])
+
+# adjoint source
+adjoint_source = flipdim(received_data_forward-received_data, 2);
+# adjoint_source = flipdim(received_data_forward, 2);
+@time backward_wavefield, received_data1 = wave_solver_2d_pml(vel_init,Nx,Ny,h,Nt,dt,pml_len,pml_alpha,receiver_coor,adjoint_source,source_coor[ind_source,:]');
+backward_wavefield = flipdim(backward_wavefield,3);
+# draw_real(backward_wavefield[:,:,700])
+
+forward_wavefield_tt = zeros(Nx, Ny, Nt);
+forward_wavefield_tt[:,:,2:Nt-1] = (forward_wavefield[:,:,1:Nt-2] - 2*forward_wavefield[:,:,2:Nt-1] + forward_wavefield[:,:,3:Nt])/(dt^2);
+S = forward_wavefield_tt .* backward_wavefield;
+S = sum(S,3);
+S = S[:,:,1];
+S = -2 ./ (vel_init).^3 .* S;
+S = S ./ maximum(abs.(S));
+
+draw_real(S)
---
+++ time_test4.jl
@@ -0,0 +1,21 @@
+# This file is for the steepest gradient method and line search algorithm.
+
+include("model_parameter.jl");
+include("2d_wave_solver.jl");
+include("FWI_time.jl")
+
+# Make data
+true_wavefield, received_data = make_data(vel_true,Nx,Ny,h,Nt,dt,pml_len,pml_alpha,source_coor,source_vec,receiver_coor);
+true_wavefield = [];
+# Source Loop
+iter_time = 3;
+for iter_main = 1:iter_time
+    println("Main iter time: ", iter_main)
+    S, received_data_forward = source_loop(vel_init, Nx, Ny, h, Nt, dt, pml_len, pml_alpha, source_coor, source_vec, receiver_coor, received_data);
+
+    alpha = line_search(vel_init, S, 100, vel_true,received_data_forward, received_data, Nx,Ny,h,Nt,dt,pml_len,pml_alpha,source_coor,source_vec,receiver_coor)
+
+    vel_init = vel_init - alpha * S;
+    # draw_real(vel_init)
+    # draw_real(S)
+end
